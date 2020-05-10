e731fb22b08edf6cf244e493fec2244e1c73cf69
Adewale Azeez <azeezadewale98@gmail.com>
2020-05-11 00:38:19 +0100

    resolve travis CI

--- deployghpage.sh
+++ deployghpage.sh
@@ -1,6 +1,5 @@
 
 mkdir -p dist
-ls
 cd dist/
 git clone -b gh-pages https://github.com/exoticlibraries/exoticlibraries.github.io.git
 cd exoticlibraries.github.io/
@@ -8,5 +7,5 @@ cp -r ../../build/html/* ./
 git config --local user.email "azeezadewale98@gmail.com"
 git config --local user.name "travis-ci.org"
 git add .; git commit -m "Travis build=${TRAVIS_BUILD_NUMBER}. Update Documentation from Travis CI"
-git push https://Thecarisma:${GITHUB_TOKEN}@github.com/exoticlibraries/exoticlibraries.github.io.git HEAD:master;
+git push -f https://Thecarisma:${GITHUB_TOKEN}@github.com/exoticlibraries/exoticlibraries.github.io.git HEAD:master;
 cd ../
